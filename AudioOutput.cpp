3ac773047b6bd79030fa40274dbdb44fec447d89
djennir <catharsis.cucumberch@gmail.com>
2025-02-11 01:06:02 +0700

    Button name changing

--- AudioOutput.cpp
+++ AudioOutput.cpp
@@ -49,7 +49,11 @@ void AudioOutput::render() {
             // ed::BeginPin(outputPinId, ed::PinKind::Output);
             //     ImGui::Text("Signal Out");
             // ed::EndPin();
-            if (ImGui::Button("Button")){
+            std::string buttonName = "Play";
+            if (isPlaying){
+                buttonName = "Stop";
+            }
+            if (ImGui::Button(buttonName.c_str())){
                 isPlaying = not(isPlaying);
             }
         ed::EndNode();
