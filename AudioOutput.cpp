f8bb01d4ecb3e2701b1285957eb6824d0d07e40e
djennir <catharsis.cucumberch@gmail.com>
2025-02-11 00:50:47 +0700

    simple button

--- AudioOutput.cpp
+++ AudioOutput.cpp
@@ -25,7 +25,7 @@ AudioOutput::AudioOutput() {
 
 void AudioOutput::audioCallback(void* userdata, Uint8* stream, int len) {
     AudioOutput* audioOutput = static_cast<AudioOutput*>(userdata);
-    if (audioOutput && audioOutput->inputModule) {
+    if (audioOutput && audioOutput->inputModule && audioOutput->isPlaying) {
         // Если есть ссылка на inputModule и флаг воспроизведения включен
         audioOutput->inputModule->process(stream, len);
     } else {
@@ -49,6 +49,9 @@ void AudioOutput::render() {
             // ed::BeginPin(outputPinId, ed::PinKind::Output);
             //     ImGui::Text("Signal Out");
             // ed::EndPin();
+            if (ImGui::Button("Button")){
+                isPlaying = not(isPlaying);
+            }
         ed::EndNode();
         m_FirstFrame = 0;
 }
