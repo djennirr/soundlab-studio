29694a3d1532873113a580be3b46d75e9bd57b6d
Grigory Chernikov <g.chernikov@g.nsu.ru>
2025-04-09 20:15:36 +0700

    debug

---
+++ debug.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+
+cmake ..
+make
+
+cd bin
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Запуск с lldb на macOS..."
+    lldb ./main
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Запуск с gdb на Ubuntu..."
+    gdb ./main
+else
+    echo "Неизвестная операционная система. Используйте macOS или Ubuntu.)))))))))))))))))))))"
+    exit 1
+fi
