c7c63a860a892148a25224fd6ebb0dd96cd644c1
champion <n.laptev1@g.nsu.ru>
2025-05-19 13:19:07 +0700

    я забыл отформотировать

--- src/Oscillator.cpp
+++ src/Oscillator.cpp
@@ -215,13 +215,14 @@ void Oscillator::generateTriangleWave(AudioSample* stream, int length) {
 
     for (int i = 0; i < length; i += 1) {
         double value;
-if (phase < period / 2) {
-    value = (2.0 * AMPLITUDE_I * phase) / (period / 2) - AMPLITUDE_I;
-} else {
-    value = (2.0 * AMPLITUDE_I * (1.0 - (phase - period / 2) / (period / 2))) - AMPLITUDE_I;
-}
-value *= volume;
-stream[i] = static_cast<AudioSample>(value);
+    if (phase < period / 2) {
+        value = (2.0 * AMPLITUDE_I * phase) / (period / 2) - AMPLITUDE_I;
+    } else {
+        value = (2.0 * AMPLITUDE_I * (1.0 - (phase - period / 2) / (period / 2))) - AMPLITUDE_I;
+    }
+    
+    value *= volume;
+    stream[i] = static_cast<AudioSample>(value);
 
         // stream[i + 1] = static_cast<AudioSample>(value); // Правый канал
 
