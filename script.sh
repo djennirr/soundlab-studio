059bccd8a6e35761043c3dfabfc79cb0613c0e01
Grigory Chernikov <g.chernikov@g.nsu.ru>
2025-05-19 16:40:52 +0700

    superdupermegafix

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ set -e
 
 conan install . --output-folder=build --build=missing
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=build/Release/generators/conan_toolchain.cmake
 cmake --build .
 cd bin
 ./main
