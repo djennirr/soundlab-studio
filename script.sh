1aebd4e672307e190ad5845bc7a2abdb6fe4aeb9
Grigory Chernikov <g.chernikov@g.nsu.ru>
2025-05-19 15:14:52 +0700

    conan

--- script.sh
+++ script.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+set -e
+
 mkdir -p build
 cd build
-cmake ..
-make
+
+# Указываем install-folder и build-folder — чтобы всё оставалось в build/
+conan install .. \
+    --output-folder=. \
+    --build=missing
+
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+cmake --build .
+
 cd bin
 ./main
